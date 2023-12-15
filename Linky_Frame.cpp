87ad8678294f7c2f811cd1a654a181e2a4f4faa8
myegoine <florian.guyot@gmail.com>
2023-12-15 22:53:01 +0100

    Create Linky_Frame.cpp

---
+++ Linky_Frame.cpp
@@ -0,0 +1,27 @@
+Linky_Frame::Linky_Frame() {
+    init();
+}
+
+void Linky_Frame::init() {
+    memset(_frame, 0, BUFFER_SIZE);
+    _size = 0;
+}
+
+bool Linky_Frame::isComplete() {
+  return (_size > 0 && _frame[_size - 1] == ETX);
+}
+
+bool Linky_Frame::isError() {
+  return (_size > 0 && _frame[_size - 1] == EOT || _size == BUFFER_SIZE);
+}
+
+void Linky_Frame::addByte(char byte) {
+  if (_size == 0 && byte != STX) {
+    return;
+  }
+  _frame[_size++] = byte;
+}
+
+char *Linky_Frame::getFrame() {
+  return _frame;
+}
